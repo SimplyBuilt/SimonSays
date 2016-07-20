dadc4d26bbbd7e68c757d51094e19db0d9e6b002
Cameron Craig <sentrathis96@gmail.com>
2016-07-20 16:28:07 -0700

    Get simon_says working with rails 5

--- test/rails_app/config/environments/test.rb
+++ test/rails_app/config/environments/test.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files  = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.public_file_server.enabled  = true
+  config.public_file_server.headers =  { 'Cache-Control' => 'public, max-age=3600' }
 
   config.active_support.test_order = :random
 
